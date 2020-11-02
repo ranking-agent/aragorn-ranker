281f42493fef071ad0f7a33177c41d98585c764a
Phil Owen <phillipsowen@outlook.com>
2020-11-02 09:15:21 -0500

    correcting tests, adding returned data assertions

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -1,18 +1,25 @@
 """Test correctness (publication) weighting."""
-# pylint: disable=redefined-outer-name,no-name-in-module,unused-import
-# ^^^ this stuff happens because of the incredible way we do pytest fixtures
 import json
-
 from fastapi.testclient import TestClient
-
 from ranker.server import APP
+# this will load all the json test files into global objects to use in a test
 from .fixtures import to_weight
 
 client = TestClient(APP)
 
 
 def test_weight(to_weight):
-    """Test that weight() runs without errors."""
-    response = client.post('/weight_correctness', json={
-        "message": to_weight
-    })
+    """Test that correctness() runs without errors."""
+    response = client.post('/weight_correctness', json={"message": to_weight})
+
+    # load the json
+    answer = json.loads(response.content)
+
+    # make sure the there are 3 results
+    assert (len(answer['results']) == 3)
+
+    # assert there are node bindings
+    assert (len(answer['results'][0]['node_bindings']) == 3)
+
+    # assert there are node bindings
+    assert (len(answer['results'][0]['edge_bindings']) == 7)
--- tests/test_score.py
+++ tests/test_score.py
@@ -1,11 +1,11 @@
 """Test scoring."""
-# pylint: disable=redefined-outer-name,no-name-in-module,unused-import
-# ^^^ this stuff happens because of the incredible way we do pytest fixtures
+import json
 from fastapi.testclient import TestClient
-
 from ranker.server import APP
+# this will load all the json test files into global objects to use in a test
 from .fixtures import weighted2
 
+# start a client
 client = TestClient(APP)
 
 
@@ -13,4 +13,14 @@ def test_score(weighted2):
     """Test that score() runs without errors."""
     response = client.post('/score', json={"message": weighted2})
 
-    print(response)
+    # load the json
+    answer = json.loads(response.content)
+
+    # make sure the there are 3 results
+    assert(len(answer['results']) == 3)
+
+    # assert there are node bindings
+    assert(len(answer['results'][0]['node_bindings'])== 3)
+
+    # assert there are node bindings
+    assert(len(answer['results'][0]['edge_bindings']) == 7)

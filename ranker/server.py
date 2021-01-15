c6f7bcfc809b4bed46025258d84885af344c5c4b
Phil Owen <phillipsowen@outlook.com>
2021-01-15 12:21:24 -0500

    rearranging the order of the initialization

--- ranker/server.py
+++ ranker/server.py
@@ -24,6 +24,32 @@ LOGGER = logging.getLogger(__name__)
 
 APP = FastAPI(title='ARAGORN Ranker', version='2.2.2')
 
+def log_exception(method):
+    """Wrap method."""
+    @wraps(method)
+    async def wrapper(*args, **kwargs):
+        """Log exception encountered in method, then pass."""
+        try:
+            return await method(*args, **kwargs)
+        except Exception as err:  # pylint: disable=broad-except
+            LOGGER.exception(err)
+            raise
+    return wrapper
+
+dirname = os.path.join(os.path.dirname(__file__), 'modules')
+
+operations = [
+    op[:-3]
+    for op in os.listdir(dirname)
+    if op.endswith('.py') and not op.startswith('_')
+]
+
+for operation in operations:
+    md = import_module(f"ranker.modules.{operation}")
+
+    APP.post('/' + operation, tags=["ARAGORN-Ranker"], response_model=Response, response_model_exclude_none=True)(log_exception(md.query))
+
+
 def construct_open_api_schema():
 
     if APP.openapi_schema:
@@ -72,6 +98,8 @@ def construct_open_api_schema():
 
     return open_api_schema
 
+APP.openapi_schema = construct_open_api_schema()
+
 APP.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
@@ -79,32 +107,3 @@ APP.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-dirname = os.path.join(os.path.dirname(__file__), 'modules')
-operations = [
-    op[:-3]
-    for op in os.listdir(dirname)
-    if op.endswith('.py') and not op.startswith('_')
-]
-
-
-def log_exception(method):
-    """Wrap method."""
-    @wraps(method)
-    async def wrapper(*args, **kwargs):
-        """Log exception encountered in method, then pass."""
-        try:
-            return await method(*args, **kwargs)
-        except Exception as err:  # pylint: disable=broad-except
-            LOGGER.exception(err)
-            raise
-    return wrapper
-
-
-for operation in operations:
-    md = import_module(f"ranker.modules.{operation}")
-
-    APP.post('/' + operation, response_model=Response, response_model_exclude_none=True)(log_exception(md.query))
-
-
-APP.openapi_schema = construct_open_api_schema()

fc5e0ca5ec90cc57db8c6f395c8d3101637633f7
Phil Owen <phillipsowen@outlook.com>
2021-04-16 11:47:30 -0400

    adding AC redis methods, added imports...

--- ranker/modules/weight_novelty.py
+++ ranker/modules/weight_novelty.py
@@ -1,12 +1,13 @@
 """Novelty."""
-import copy
 import logging
 import os
-import time
+import json
 import uuid
+import redis
+import itertools
 
 from collections import defaultdict
-from reasoner_pydantic import Response, Message
+from reasoner_pydantic import Response
 from ranker.shared.qgraph_compiler import NodeReference, EdgeReference
 from ranker.shared.util import batches
 from ranker.shared.neo4j_ import Neo4jDatabase
@@ -19,6 +20,28 @@ AC_CACHE_PORT = os.environ.get('AC_CACHE_PORT', '1234')
 NEO4J_URL = os.environ.get('NEO4J_URL', 'http://localhost:7474')
 NEO4J_USER = os.environ.get('NEO4J_USER', 'neo4j')
 NEO4J_PASSWORD = os.environ.get('NEO4J_PASSWORD', 'pword')
+
+
+def create_nodes_to_links(allnodes):
+    p = get_redis_pipeline(0)
+    # Create a dict from node->links by looking up in redis.  Each link is a potential node to add.
+    # This is across all opportunities as well
+    # Could pipeline
+    nodes_to_links = {}
+    for group in grouper(1000, allnodes.keys()):
+        for node in group:
+            p.get(node)
+        linkstrings = p.execute()
+        for node, linkstring in zip(group, linkstrings):
+            if linkstring is None:
+                print(node)
+            if linkstring is None:
+                links = []
+            else:
+                links = json.loads(linkstring)
+            nodes_to_links[node] = links
+    return nodes_to_links
+
 
 def get_rgraph(result, message):
     """Get "ranker" subgraph."""
@@ -85,6 +108,74 @@ def get_rgraph(result, message):
     }
 
 
+def get_redis_pipeline(dbnum):
+    typeredis = redis.Redis(host=AC_CACHE_HOST, port=int(AC_CACHE_PORT), db=dbnum)
+
+    p = typeredis.pipeline()
+
+    return p
+
+
+def get_link_counts(unique_links):
+    # Now we are going to hit redis to get the counts for all of the links.
+    # our unique_links are the keys
+    p = get_redis_pipeline(2)
+    lcounts = {}
+    for ulg in grouper(1000, unique_links):
+        for ul in ulg:
+            p.get(str(ul))
+        ns = p.execute()
+        for ul, n in zip(ulg, ns):
+            try:
+                lcounts[ul] = int(n)
+            except Exception as e:
+                print('Failure')
+                print(ul)
+                raise e
+    return lcounts
+
+
+def grouper(n, iterable):
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, n))
+        if not chunk:
+            break
+        yield chunk
+
+
+def uniquify_links(nodes_to_links, opportunities):
+    # A link might occur for multiple nodes and across different opportunities
+    # Create the total unique set of links
+    unique_links = set()
+    unique_link_nodes = set()
+    io = 0
+    for opportunity in opportunities:
+        # print('new op', io, dt.now())
+        io += 1
+        kn = opportunity.get_kg_ids()
+        # print('',len(kn))
+        seen = set()
+        for n in kn:
+            # if len(nodes_to_links[n]) > 10000:
+            #    print(' ',n,len(nodes_to_links[n]),opportunity.get_qg_semantic_type())
+            for link in nodes_to_links[n]:
+                # The link as defined uses the input node as is_source, but the lookup into redis uses the
+                # linked node as the is_source, so gotta flip it
+                lplus = link + [opportunity.get_qg_semantic_type()]
+                lplus[2] = not lplus[2]
+                tl = tuple(lplus)
+                if tl in seen:
+                    # if lplus[0] == 'NCBIGene:355':
+                    #    print(io, tl)
+                    unique_links.add(tl)
+                    unique_link_nodes.add(tl[0])
+                else:
+                    seen.add(tl)
+        # print('','end',dt.now())
+    return unique_link_nodes, unique_links
+
+
 async def query(response: Response, *, exclude_sets=False) -> Response:
     """Compute informativeness weights for edges."""
     message = response.message.dict()
@@ -136,7 +227,9 @@ async def query(response: Response, *, exclude_sets=False) -> Response:
                 'type': 'named_thing'
             })
             anchor_node_reference = str(anchor_node_reference)
-            base = f"MATCH ({anchor_node_reference}) "
+
+            # base = f"MATCH ({anchor_node_reference}) "
+
             for jdx, (qlink, redge_ids) in enumerate(plan.items()):
                 cypher_counts = []
                 qedge_id, qtarget_id = qlink
@@ -147,12 +240,16 @@ async def query(response: Response, *, exclude_sets=False) -> Response:
                     **qnode_map[qtarget_id],
                     'id': count_id,
                 })
+
                 if qedge['source_id'] == qtarget_id:
                     source_reference = anon_node_reference
                     target_reference = anchor_node_reference
                 elif qedge['target_id'] == qtarget_id:
                     source_reference = anchor_node_reference
                     target_reference = anon_node_reference
+
+                # TODO: source_reference and target_reference referenced before assignent
+
                 cypher_counts.append(f"{anon_node_reference.name}: count(DISTINCT {anon_node_reference.name})")
                 count_to_redge[count_id] = redge_ids
                 sets.append(f'MATCH ({source_reference}){edge_reference}({target_reference})' + ' RETURN {' + ', '.join(cypher_counts) + '} as output')

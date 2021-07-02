031a06a4b63d913ead8da662e181dad04941c430
Chris Bizon <bizon@email.unc.edu>
2021-07-01 21:19:58 -0400

    Upgraded Tests

--- tests/test_score.py
+++ tests/test_score.py
@@ -3,7 +3,8 @@ import json
 from fastapi.testclient import TestClient
 from ranker.server import APP
 # this will load all the json test files into global objects to use in a test
-from .fixtures import weighted2, weighted_answer, schizo, treatsSchizophreniaw, weighted_set
+from .fixtures import score_test, weighted2, weighted_answer, schizo, treatsSchizophreniaw, weighted_set
+from pytest import approx
 
 # start a client
 client = TestClient(APP)
@@ -61,3 +62,39 @@ def test_score_schizo(treatsSchizophreniaw):
     answer = resp['message']['results']
 
     assert response.status_code == 200
+
+def test_score_set(weighted_set):
+    response = client.post('/score', json=weighted_set)
+    resp = response.json()
+    answer = resp['message']['results']
+    print(answer[0]['score'])
+    assert response.status_code == 200
+
+def test_score_set_symmetry(weighted_set):
+    """For our sets, it should not matter the order in which values are applied to edges"""
+    edge_bindings = weighted_set['message']['results'][0]['edge_bindings']['e01']
+    #Set the weights one way, score it
+    edge_bindings[0]['attributes'][0]['value'] = 2
+    edge_bindings[1]['attributes'][0]['value'] = 1
+    response = client.post('/score', json=weighted_set)
+    resp = response.json()
+    answer = resp['message']['results']
+    score_one = answer[0]['score']
+    #Reverse the weights, score it
+    edge_bindings[0]['attributes'][0]['value'] = 1
+    edge_bindings[1]['attributes'][0]['value'] = 2
+    response = client.post('/score', json=weighted_set)
+    resp = response.json()
+    answer = resp['message']['results']
+    score_two = answer[0]['score']
+    assert score_one == approx(score_two)
+
+
+def test_basic_scoring(score_test):
+    response = client.post('/score', json=score_test)
+    resp = response.json()
+    answer = resp['message']['results']
+    assert response.status_code == 200
+    assert len(answer) == 2
+    assert answer[0]['score'] == approx(2)
+    assert answer[1]['score'] == approx(1)

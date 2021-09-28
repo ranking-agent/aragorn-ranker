9264b668a47c23159f68d179700840bc570c637e
Phil Owen <phillipsowen@outlook.com>
2021-09-28 14:04:50 -0400

    fixing test

--- tests/test_omnicorp_overlay.py
+++ tests/test_omnicorp_overlay.py
@@ -23,7 +23,7 @@ def test_omnicorp_overlay(omnicorp_input):
     assert(len(answer['message']['results'][0]['node_bindings']) == 2)
 
     # assert there are 2 edge bindings.  The original one, and the one that came out of omnicorp
-    assert(len(answer['message']['results'][0]['edge_bindings']) == 1)
+    assert(len(answer['message']['results'][0]['edge_bindings']) == 2)
 
 def test_omnicorp_overlay_with_set(property_coalesce):
     """Test that omnicorp_overlay() runs without errors."""
@@ -37,5 +37,5 @@ def test_omnicorp_overlay_with_set(property_coalesce):
     #This question is a -> set(b), so the edge bindings shoudl have one qgid "ab" and then a bunch of "s*"
     #There are 9 of them, for 10 in total
 
-    assert(len(answer['message']['results'][0]['edge_bindings']) == 2)
+    assert(len(answer['message']['results'][0]['edge_bindings']) == 1)
     assert(len(answer['message']['results'][0]['edge_bindings']['ab']) == 10)

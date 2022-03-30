fe54abf7c0313b55b3ac9abd1a9aaa5d18a29cc7
Sarah Seitanakis <sseitanakis@gmail.com>
2022-03-30 14:41:31 -0400

    create aux file & fnc for get_node_pmids

---
+++ ranker/aux/omnicorp_get_node_pmids.py
@@ -0,0 +1,27 @@
+"""Literature related to node."""
+from fastapi.responses import JSONResponse
+from ranker.shared.omnicorp import OmnicorpSupport
+from ranker.shared.util import Curie
+
+
+async def get_node_pmids(curies: Curie):
+    """Find the pubmed ids where the specified node appears.
+
+    inputs: curies (CurieList): a dictionary contianing the key "curies"
+            whose value is a list of curie-strings (see util.py). The length of
+            this list is exactly one.
+            Example:
+            curies = {
+                "curies": ['MESH:D014867']
+            }
+    """
+    nodeList = curies.curies
+    if len(nodeList) != 1:
+        raise ValueError(f'nodeList must contain exactly 1 node: nodeList = {nodeList}')
+
+    async with OmnicorpSupport() as supporter:
+        shared_pubmed_ids = await supporter.node_pmids(nodeList[0])
+        status_code = 200
+
+    # return the result to the caller
+    return JSONResponse(content=shared_pubmed_ids, status_code=status_code)

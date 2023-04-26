7c85fddd847e071fd7acac9abcf1b824e8b9860d
cbizon <bizon@renci.org>
2023-04-26 15:18:01 -0400

    updated overlay

--- tests/test_redisgraph.py
+++ tests/test_redisgraph.py
@@ -8,3 +8,12 @@ def test_get_pcounts():
     assert "baddie" not in result
     assert result["CHEBI:92293"] == 9
     assert result["MONDO:0005249"] == 128504
+
+def test_get_shared_counts():
+    keys = [["CHEBI:92293", "MONDO:0005249"], ["baddie", "MONDO:0005249"], ["NCBITaxon:2697049","MONDO:0004730"]]
+    cache = Cache()
+    results = cache.mquery(keys,"OMNICORP",
+              "as q MATCH (a:CURIE {concept:q[0]})-[x]-(b:CURIE {concept:q[1]}) return q[0],q[1],x.publication_count")
+    assert ("CHEBI:92293", "MONDO:0005249") not in results
+    assert ("baddie", "MONDO:0005249") not in results
+    assert results[("NCBITaxon:2697049", "MONDO:0004730")] == 114
